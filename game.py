e71be9cd860576dca58a53ba70441ccf1c95309f
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-11 20:34:16 +0530

    (feat): move to turtle (built-in module)

--- game.py
+++ game.py
@@ -1,24 +1,17 @@
-import pygame
-
-pygame.init()
-
-dis = pygame.display.set_mode((900, 600))
-pygame.display.set_caption("PYris")
-clock = pygame.time.Clock()
+import turtle
 
 
+win = turtle.Screen()
+win.title("Pong by @abhinav.robinson")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+
+# START GAME LOOP
 def main():
-    # GAME LOOP START
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-    pygame.display.update()
-
-    dis.fill(0)
-    clock.tick(30)
+        win.update()
 
 
 main()
